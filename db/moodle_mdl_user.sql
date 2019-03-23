d49318b3e4e8876c491b5243e155db9d7e21b2c1
senior <calisir.bugra@gmail.com>
2019-03-23 12:30:44 +0200

    database export

---
+++ db/moodle_mdl_user.sql
@@ -0,0 +1,117 @@
+-- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
+--
+-- Host: localhost    Database: moodle
+-- ------------------------------------------------------
+-- Server version	5.7.25-0ubuntu0.16.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `mdl_user`
+--
+
+DROP TABLE IF EXISTS `mdl_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mdl_user` (
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `auth` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'manual',
+  `confirmed` tinyint(1) NOT NULL DEFAULT '0',
+  `policyagreed` tinyint(1) NOT NULL DEFAULT '0',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `suspended` tinyint(1) NOT NULL DEFAULT '0',
+  `mnethostid` bigint(10) NOT NULL DEFAULT '0',
+  `username` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `idnumber` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `firstname` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `lastname` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `emailstop` tinyint(1) NOT NULL DEFAULT '0',
+  `icq` varchar(15) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `skype` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `yahoo` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `aim` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `msn` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `phone1` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `phone2` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `institution` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `department` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `city` varchar(120) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `country` varchar(2) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `lang` varchar(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'en',
+  `calendartype` varchar(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'gregorian',
+  `theme` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `timezone` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '99',
+  `firstaccess` bigint(10) NOT NULL DEFAULT '0',
+  `lastaccess` bigint(10) NOT NULL DEFAULT '0',
+  `lastlogin` bigint(10) NOT NULL DEFAULT '0',
+  `currentlogin` bigint(10) NOT NULL DEFAULT '0',
+  `lastip` varchar(45) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `secret` varchar(15) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `picture` bigint(10) NOT NULL DEFAULT '0',
+  `url` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `description` longtext COLLATE utf8_unicode_ci,
+  `descriptionformat` tinyint(2) NOT NULL DEFAULT '1',
+  `mailformat` tinyint(1) NOT NULL DEFAULT '1',
+  `maildigest` tinyint(1) NOT NULL DEFAULT '0',
+  `maildisplay` tinyint(2) NOT NULL DEFAULT '2',
+  `autosubscribe` tinyint(1) NOT NULL DEFAULT '1',
+  `trackforums` tinyint(1) NOT NULL DEFAULT '0',
+  `timecreated` bigint(10) NOT NULL DEFAULT '0',
+  `timemodified` bigint(10) NOT NULL DEFAULT '0',
+  `trustbitmask` bigint(10) NOT NULL DEFAULT '0',
+  `imagealt` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastnamephonetic` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `firstnamephonetic` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `middlename` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `alternatename` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `mdl_user_mneuse_uix` (`mnethostid`,`username`),
+  KEY `mdl_user_del_ix` (`deleted`),
+  KEY `mdl_user_con_ix` (`confirmed`),
+  KEY `mdl_user_fir_ix` (`firstname`),
+  KEY `mdl_user_las_ix` (`lastname`),
+  KEY `mdl_user_cit_ix` (`city`),
+  KEY `mdl_user_cou_ix` (`country`),
+  KEY `mdl_user_las2_ix` (`lastaccess`),
+  KEY `mdl_user_ema_ix` (`email`),
+  KEY `mdl_user_aut_ix` (`auth`),
+  KEY `mdl_user_idn_ix` (`idnumber`),
+  KEY `mdl_user_fir2_ix` (`firstnamephonetic`),
+  KEY `mdl_user_las3_ix` (`lastnamephonetic`),
+  KEY `mdl_user_mid_ix` (`middlename`),
+  KEY `mdl_user_alt_ix` (`alternatename`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=COMPRESSED COMMENT='One record for each person';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mdl_user`
+--
+
+LOCK TABLES `mdl_user` WRITE;
+/*!40000 ALTER TABLE `mdl_user` DISABLE KEYS */;
+INSERT INTO `mdl_user` VALUES (1,'manual',1,0,0,0,1,'guest','$2y$10$tvCzYonqIR7pl4p8Sx3HTOjWiSLQOpKxrlrkoJB0Z/cXLHizLhBJi','','Guest user',' ','root@localhost',0,'','','','','','','','','','','','','en','gregorian','','99',0,0,0,0,'','',0,'','This user is a special user that allows read-only access to some courses.',1,1,0,2,1,0,0,1553332642,0,NULL,NULL,NULL,NULL,NULL),(2,'manual',1,0,0,0,1,'admin','$2y$10$hEbFdAcWxqeBSkqEDRkCfOqcJYpnw8mrknLnGjy4yLDbpk4qqJ.Je','','Admin','User','mustafa-cihan@hotmail.com',0,'','','','','','','','','','','','','en','gregorian','','99',1553332989,1553336193,0,1553332989,'0:0:0:0:0:0:0:1','',0,'','',1,1,0,1,1,0,0,1553333193,0,NULL,'','','',''),(3,'email',1,0,0,0,1,'mustafa','$2y$10$718l6QL7NIaKB55zlg7BJ.HXDaBndU2KnawAUH2zvamn0YYSKVgpy','','Mustafa','Gürbüz','cihan.gurbuz@metu.edu.tr',0,'','','','','','','','','','','','CH','en','gregorian','','99',0,0,0,0,'','',0,'','',1,1,0,2,1,0,1553335944,1553335944,0,'','','','',''),(4,'manual',1,0,0,0,1,'alper','$2y$10$SNffHKgVfv/yYvuyuFY5veqUwTg8W1syNBpFFxJ.FSCgpKu3ED93S','','Alper','Çalışır','calisir.alper@metu.edu.tr',0,'','','','','','','','','','','','TR','en','gregorian','','99',0,0,0,0,'','',0,'','',1,1,0,2,1,0,1553336026,1553336026,0,'','','','',''),(5,'manual',1,0,0,0,1,'sinan','$2y$10$NZjjYWSieGRL0CqkKvoo3u/vJ6nyLJQ8OoAehMwr1tS4SteWG8/gW','','Sinan','Ulusoy','snnlsy@gmail.com',0,'','','','','','','','','','','','TR','en','gregorian','','99',0,0,0,0,'','',0,'','',1,1,0,2,1,0,1553336129,1553336129,0,'','','','','');
+/*!40000 ALTER TABLE `mdl_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-03-23 12:28:58
